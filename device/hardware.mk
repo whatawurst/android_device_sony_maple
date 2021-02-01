45c5e37bd95ff4cfef1a6d87bc5f7e5d84df40c7
derfelot <derfelot.git@gmail.com>
2021-02-01 20:17:18 +0100

    maple: Import inclusion of nfc service from yoshino-common
    Japanese variants have a modified service to work with cxd224, so they need to use the stock service and .rc.
    
    Import this in the non-Japanese devices instead.

--- device/hardware.mk
+++ device/hardware.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # HW crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
@@ -11,6 +25,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.1-service
 
 # RIL
 PRODUCT_PACKAGES += \
