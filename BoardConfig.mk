99d1e0251ea29e6e8f9726d93e0c86b27a3a6bc0
derfelot <derfelot.git@gmail.com>
2021-02-10 02:21:48 +0100

    maple: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+### INHERIT FROM YOSHINO-COMMON
+include device/sony/yoshino-common/BoardConfigPlatform.mk
+
+DEVICE_PATH := device/sony/maple
+
+PRODUCT_PLATFORM := yoshino
+
+### BOOTLOADER
+TARGET_BOOTLOADER_BOARD_NAME := G8141
+
+### KERNEL
+BOARD_KERNEL_CMDLINE += androidboot.hardware=maple
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+
+### PARTITIONS
+# See also /proc/partitions on the device
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 7707033600
+# Reserve space for data encryption (54587760640-32768)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 54587727872
+
+# VENDOR
+TARGET_COPY_OUT_VENDOR := system/vendor
+
+### DISPLAY
+TARGET_SCREEN_DENSITY := 480
+
+### TWRP
+
+## FSTAB
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/twrp.fstab
+
+## VERSION
+TW_DEVICE_VERSION := 1
---
+++ README.md
@@ -0,0 +1,7 @@
+Device configuration for Sony Xperia XZ Premium (maple)
+========================================================
+
+Description
+-----------
+
+This repository is for TWRP on Sony Xperia XZ Premium (maple).
