06662f2ec681f3dc9ca7e092df9768b624c36a92
derfelot <derfelot.git@gmail.com>
2022-08-12 19:55:15 +0200

    maple: extract: Replace xml version 2.0 with 1.0
    Change-Id: I7b672b8f5c66b1a56d118c70765a6faa6e8e55e4

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,10 @@ sed -i 's/\/lib\/firmware\/ufs/\/etc\/firmware\/ufs/g' "${DEVICE_ROOT}"/vendor/b
 # Add a restorecon for /persist/wlan to taimport_vendor.rc
 sed -i '4 a\    restorecon /persist/wlan' "${DEVICE_ROOT}"/vendor/etc/init/taimport_vendor.rc
 
+# Change xml version from 2.0 to 1.0
+sed -i 's/version\=\"2\.0\"/version\=\"1\.0\"/g' "${DEVICE_ROOT}"/product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml
+sed -i 's/version\=\"2\.0\"/version\=\"1\.0\"/g' "${DEVICE_ROOT}"/product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml
+
 # Move drm firmware to vendor
 sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${DEVICE_ROOT}"/vendor/lib/libwvtee.so
 sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${DEVICE_ROOT}"/vendor/lib64/libwvtee.so
